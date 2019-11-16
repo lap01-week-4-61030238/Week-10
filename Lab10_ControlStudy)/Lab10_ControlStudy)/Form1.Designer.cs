26f94c07aec16730e4412ad6779bd56cdd781062
“Saya” <61030238@kmitl.ac.th>
2019-11-16 14:25:28 +0700

    1

--- Lab10_ControlStudy)/Lab10_ControlStudy)/Form1.Designer.cs
+++ Lab10_ControlStudy)/Lab10_ControlStudy)/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp1
+﻿namespace Lab10_ControlStudy_
 {
     partial class Form1
     {
@@ -28,26 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(406, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnHello_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightGreen;
-            this.ClientSize = new System.Drawing.Size(500, 300);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MaximumSize = new System.Drawing.Size(1700, 1700);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.Text = "Hello World";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "Form1";
+            this.MaximumSizeChanged += new System.EventHandler(this.Form1_MaximumSizeChanged);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ Lab10_ControlStudy)/Lab10_ControlStudy)/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10_ControlStudy_
+{
+    public partial class Form1 : Form
+    {
+        Button btnHello;
+        public Form1()
+        {
+            InitializeComponent();
+            btnHello = new Button();
+            btnHello.Location = new Point(20, 20);
+            btnHello.Text = "Hello";
+            this.Controls.Add(btnHello);
+        }
+
+        private void Form1_MaximumSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
