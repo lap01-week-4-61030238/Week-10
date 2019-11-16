587738b9737e33debf2e4eefb917cdc1ad307d41
“Saya” <61030238@kmitl.ac.th>
2019-11-16 14:53:50 +0700

    3

--- Lab10_ControlStudy)/Lab10_ControlStudy)/Form1.cs
+++ Lab10_ControlStudy)/Lab10_ControlStudy)/Form1.cs
@@ -7,6 +7,7 @@ namespace Lab10_ControlStudy_
     public partial class Form1 : Form
     {
         Button btnHello;
+        Color btnBackColor;
         public Form1()
         {
             InitializeComponent();
@@ -15,11 +16,23 @@ namespace Lab10_ControlStudy_
             btnHello.Text = "Hello";
             this.Controls.Add(btnHello);
             btnHello.Click += btnHello_Click;
+            btnBackColor = btnHello.BackColor;
+            btnHello.MouseEnter += BtnHello_MouseEnter;
+            btnHello.MouseLeave += BtnHello_MouseLeave;
+
         }
 
         private void Form1_MaximumSizeChanged(object sender, EventArgs e)
         {
 
+        }
+        private void BtnHello_MouseLeave(object sender, EventArgs e)
+        {
+            btnHello.BackColor = btnBackColor;
+        }
+        private void BtnHello_MouseEnter(object sender, EventArgs e)
+        {
+            btnHello.BackColor = Color.Green;
         }
 
         private void btnHello_Click(object sender, EventArgs e)
